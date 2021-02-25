1d473d3e54a69339c1a6838a4160a76b168a29aa
Brett Anda <brett@brettanda.ca>
2021-02-25 04:24:53 -0700

    fixed dbl notifications

--- functions/dbl.py
+++ functions/dbl.py
@@ -1,12 +1,12 @@
-import dbl
+import dbl,logging
 
-from discord.ext import commands
+from discord.ext.commands import Cog
 
 import os#,sys
 # sys.path.insert(1, os.path.join(sys.path[0], '..'))
 # from functions import *
 
-class TopGG(commands.Cog):
+class TopGG(Cog):
   """Handles interactions with the top.gg API"""
 
   def __init__(self,bot):
@@ -14,8 +14,10 @@ class TopGG(commands.Cog):
     self.token = os.getenv("TOKENDBL")
     self.dblpy = dbl.DBLClient(self.bot,self.token,autopost=True)
 
+  @Cog.listener()
   async def on_guild_post():
     print("Server count posted successfully")
+    logging.info("Server count posted successfully")
   
 def setup(bot):
   bot.add_cog(TopGG(bot))
