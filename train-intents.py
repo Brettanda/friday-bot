34299617e39d9268fc7ab19a2f3c4a16775efd85
Brett Anda <brett@brettanda.ca>
2021-02-25 14:01:51 -0700

    fixed friday ml (mostly)

--- train-intents.py
+++ train-intents.py
@@ -89,19 +89,21 @@ train_y = list(training[:,1])
 # Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
 # equal to number of intents to predict output intent with softmax
 model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dense(256, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.25))
 model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.25))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
 # Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
-# sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-# model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Fit the model
-model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1,shuffle=False)
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1,shuffle=True)
 model.summary()
 
 model.save('ml/models/intent_model.h5')
