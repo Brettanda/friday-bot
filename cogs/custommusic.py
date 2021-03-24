626fe47a2be5feaec932178cc369e698870521bf
Brett Anda <brett@brettanda.ca>
2021-03-24 02:39:47 -0600

    fixed imports 6

--- cogs/custommusic.py
+++ cogs/custommusic.py
@@ -1,6 +1,7 @@
 import validators,json
 from discord.ext import commands
 
+from functions import embed,MessageColors,mydb_connect,query,ignore_guilds,exceptions
 from cogs.music import can_play
 
 class CustomMusic(commands.Cog):
