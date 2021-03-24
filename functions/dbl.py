96f0513c084c8a99395e05aaab14c277499975c0
Brett Anda <brett@brettanda.ca>
2021-03-24 02:16:50 -0600

    better logging for dbl

--- functions/dbl.py
+++ functions/dbl.py
@@ -1,23 +1,36 @@
-import dbl,logging
+import dbl,logging,os
 
-from discord.ext.commands import Cog
+from discord.ext import commands,tasks
 
-import os#,sys
-# sys.path.insert(1, os.path.join(sys.path[0], '..'))
-# from functions import *
-
-class TopGG(Cog):
+class TopGG(commands.Cog):
   """Handles interactions with the top.gg API"""
 
   def __init__(self,bot):
     self.bot = bot
     self.token = os.getenv("TOKENDBL")
-    self.dblpy = dbl.DBLClient(self.bot,self.token,autopost=True)
+    self.dblpy = dbl.DBLClient(self.bot,self.token,autopost=False)
+    self.update_stats.start()
 
-  @Cog.listener()
-  async def on_guild_post(self):
-    print("Server count posted successfully")
-    logging.info("Server count posted successfully")
-  
+  def cog_unload(self):
+    self.update_stats.cancel()
+
+  @tasks.loop(minutes=30.0)
+  async def update_stats(self):
+    await self.bot.wait_until_ready()
+    logger.info("Updating DBL stats")
+    try:
+      await self.dblpy.post_guild_count(guild_count=len(self.bot.guilds),shard_count=self.bot.shard_count,shard_id=self.bot.shard_id)
+      logger.info("Server count posted successfully")
+    except Exception as e:
+      logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
+
+
+  @commands.Cog.listener()
+  async def on_dbl_vote(self,data):
+    logger.info('Received an upvote')
+    logger.info(data)
+
 def setup(bot):
+  global logger
+  logger = logging.getLogger('dbl')
   bot.add_cog(TopGG(bot))
