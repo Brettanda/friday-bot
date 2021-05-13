5aa9f7b2ec6c0878e4a4788bbafe29d9254c8769
Brett Anda <brett@brettanda.ca>
2021-05-13 17:37:46 -0600

    fixed restart not showing live count for vc's

--- cogs/dev.py
+++ cogs/dev.py
@@ -117,13 +117,13 @@ class Dev(GlobalCog, command_attrs=dict(hidden=True)):
       return
 
     self.bot.restartPending = True
-    stat = await ctx.reply(embed=embed(title="Pending"))
-    if len(songqueue) > 0 and force is False:
-      await stat.edit(embed=embed(title=f"{len(songqueue)} guilds are playing music"))
-      while len(songqueue) > 0:
-        await stat.edit(embed=embed(title=f"{len(songqueue)} guilds are playing music"))
+    stat = await ctx.reply(embed=embed(title="Pending"), delete_after=None)
+    if len(self.bot.voice_clients) > 0 and force is False:
+      await stat.edit(embed=embed(title=f"{len(self.bot.voice_clients)} guilds are playing music"))
+      while len(self.bot.voice_clients) > 0:
+        await stat.edit(embed=embed(title=f"{len(self.bot.voice_clients)} guilds are playing music"))
         await asyncio.sleep(1)
-      await stat.edit(embed=embed(title=f"{len(songqueue)} guilds are playing music"))
+      await stat.edit(embed=embed(title=f"{len(self.bot.voice_clients)} guilds are playing music"))
     # if len(songqueue) is 0 or force is True:
     thispath = os.getcwd()
     if "\\" in thispath:
