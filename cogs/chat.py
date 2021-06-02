cfff0afd4836170df40bd3ce0e8a812d1e7bfb12
Brett Anda <brett@brettanda.ca>
2021-06-02 16:53:48 -0600

    ignoring discord server error

--- cogs/chat.py
+++ cogs/chat.py
@@ -75,12 +75,14 @@ class Chat(commands.Cog):
 
     if not ctx.content.startswith(tuple(self.bot.get_prefixes())):
       noContext = ["Title of your sex tape", "Self Aware", "No U", "I'm dad"]
-      lastmessages = None
+      lastmessages, lastauthormessages = None
       try:
         lastmessages = await ctx.channel.history(limit=3, oldest_first=False).flatten()
         lastauthormessages = [message for message in await ctx.channel.history(limit=5, oldest_first=False).flatten() if message.author.id == ctx.author.id]
       except TimeoutError:
         return
+      except discord.DiscordServerError:
+        pass
       meinlastmessage = False
       # newest = None
 
