0b1189cf4b397b2ff8209b92529f06e40139a823
Brett Anda <brett@brettanda.ca>
2021-06-02 17:06:42 -0600

    fix 2 for serverinfo

--- cogs/serverinfo.py
+++ cogs/serverinfo.py
@@ -23,7 +23,7 @@ class ServerInfo(commands.Cog):
 
   async def server_info(self, ctx):
     # async with ctx.typing() if ctx.typing is not None else ctx.defer():
-    prefix, delete_after, musicchannel, defaultRole = await query(self.bot.mydb, "SELECT prefix,autoDeleteMSGs,musicChannel,defaultRole FROM servers WHERE id=%s", ctx.guild.id)[0]
+    prefix, delete_after, musicchannel, defaultRole = (await query(self.bot.mydb, "SELECT prefix,autoDeleteMSGs,musicChannel,defaultRole FROM servers WHERE id=%s", ctx.guild.id))[0]
     return dict(
         embed=embed(
             title=ctx.guild.name + " - Info",
