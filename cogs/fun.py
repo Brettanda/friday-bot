a0c1165354e09bc6d385a2b6d8750ce626c6bc53
Brett Anda <brett@brettanda.ca>
2021-06-12 17:58:55 -0600

    fix for the raw reaction 404 messages

--- cogs/fun.py
+++ cogs/fun.py
@@ -353,7 +353,11 @@ class Fun(commands.Cog):
       return
     if payload.emoji.name not in self.POLLEMOTES.values():
       return
-    message = await (self.bot.get_channel(payload.channel_id)).fetch_message(payload.message_id)
+    message = None
+    try:
+      message = await (self.bot.get_channel(payload.channel_id)).fetch_message(payload.message_id)
+    except Exception:
+      pass
     if message.author != self.bot.user:
       return
     if len(message.embeds) == 0:
