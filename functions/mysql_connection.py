c90a91bb68da491e619e91ec28ef91ec5bf055cb
Brett Anda <brett@brettanda.ca>
2021-07-05 17:24:53 -0600

    fix maybe part 3

--- functions/mysql_connection.py
+++ functions/mysql_connection.py
@@ -11,21 +11,30 @@ from . import config
 def mydb_connect():  # -> mysql.connector.pooling.MySQLConnectionPool():
   # https://www.mysqltutorial.org/python-connecting-mysql-databases/
   if len(sys.argv) > 1 and (sys.argv[1] == "--prod" or sys.argv[1] == "--production"):
-    POOLNAME = "PROD"
+    # POOLNAME = "PROD"
+    HOST = "127.0.0.1"
+    USERNAME = os.getenv("DBUSERPROD")
+    PASSWORD = os.getenv("DBPASSPROD")
     DATABASE = os.getenv("DATABASE")
   elif len(sys.argv) > 1 and sys.argv[1] == "--canary":
-    POOLNAME = "CAN"
+    # POOLNAME = "CAN"
+    HOST = os.getenv("DBHOST")
+    USERNAME = os.getenv("DBUSER")
+    PASSWORD = os.getenv("DBPASS")
     DATABASE = os.getenv("DATABASECANARY")
   else:
-    POOLNAME = "TEST"
+    # POOLNAME = "TEST"
+    HOST = os.getenv("DBHOST")
+    USERNAME = os.getenv("DBUSER")
+    PASSWORD = os.getenv("DBPASS")
     DATABASE = os.getenv("DATABASETEST")
-  mydb = mysql.connector.pooling.MySQLConnectionPool(
-      pool_name=POOLNAME,
-      pool_size=5,
-      pool_reset_session=True,
-      host=os.getenv("DBHOST"),
-      user=os.getenv("DBUSER"),
-      password=os.getenv("DBPASS"),
+  mydb = mysql.connector.MySQLConnection(
+      # pool_name=POOLNAME,
+      # pool_size=5,
+      # pool_reset_session=True,
+      host=HOST,
+      user=USERNAME,
+      password=PASSWORD,
       database=DATABASE
   )
 
@@ -35,10 +44,10 @@ def mydb_connect():  # -> mysql.connector.pooling.MySQLConnectionPool():
 # async def query(mydb: mysql.connector.MySQLConnection(), query: str, *params, rlist: bool = False) -> str or list:
 async def query(mydb, query: str, *params, rlist: bool = False) -> str or list:
   try:
-    mydb = mydb.get_connection()
+    mydb = mydb_connect()
     # if not mydb.is_connected():
     #   mydb.reconnect(attempts=2, delay=0.1)
-    mycursor = mydb.cursor(prepared=True)
+    mycursor = mydb.cursor()
     mycursor.execute(query, params)
     if "select" in query.lower():
       if "where" in query.lower() and "," not in query.lower() and '>' not in query.lower().split("where")[1] and '<' not in query.lower().split("where")[1] or "limit" in query.lower():
@@ -65,10 +74,10 @@ async def query(mydb, query: str, *params, rlist: bool = False) -> str or list:
 def non_coro_query(mydb, query: str, *params, rlist: bool = False) -> str or list:
   """Meant to placed in __init__() of cogs"""
   try:
+    mydb = mydb_connect()
     # if not mydb.is_connected():
     #   mydb.reconnect(attempts=2, delay=0.1)
-    mydb = mydb.get_connection()
-    mycursor = mydb.cursor(prepared=True)
+    mycursor = mydb.cursor()
     mycursor.execute(query, params)
     if "select" in query.lower():
       if "where" in query.lower() and "," not in query.lower() and '>' not in query.lower().split("where")[1] and '<' not in query.lower().split("where")[1] or "limit" in query.lower():
