5fe2e42e06fa2b4a701c85409280d28170b989e3
Brett Anda <brett@brettanda.ca>
2021-11-11 14:47:23 -0700

    fixed the no spice folder

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -5,10 +5,13 @@ default = [
     if com.endswith(".py") and com != "__init__.py" and com != "log.py" and com != "database.py"
 ]
 
-spice = [
-    com[:-3] for com in os.listdir("./spice/cogs")
-    if com.endswith(".py") and com != "__init__.py" and com != "log.py" and com != "database.py"
-]
+try:
+  spice = [
+      com[:-3] for com in os.listdir("./spice/cogs")
+      if com.endswith(".py") and com != "__init__.py" and com != "log.py" and com != "database.py"
+  ]
+except FileNotFoundError:
+  spice = []
 
 __all__ = (
     "default",
