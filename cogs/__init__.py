3fea2579388578b2f84fd7fc171172e209c71be2
Brett Anda <brett@brettanda.ca>
2021-11-11 14:38:48 -0700

    updated cog loading for spice

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,3 +1,16 @@
 import os
 
-default = [com[:-3] for com in os.listdir("./cogs") if com.endswith(".py") and com != "__init__.py" and com != "log.py" and com != "database.py"]
+default = [
+    com[:-3] for com in os.listdir("./cogs")
+    if com.endswith(".py") and com != "__init__.py" and com != "log.py" and com != "database.py"
+]
+
+spice = [
+    com[:-3] for com in os.listdir("./spice/cogs")
+    if com.endswith(".py") and com != "__init__.py" and com != "log.py" and com != "database.py"
+]
+
+__all__ = (
+    "default",
+    "spice",
+)
