65b6fb38edb84cc62678817132238033635b71c5
Brett Anda <brett@brettanda.ca>
2021-12-08 14:28:26 -0700

    BRUHHHHHHH

--- asdtest_launcher.py
+++ asdtest_launcher.py
@@ -5,7 +5,7 @@ import os
 from discord.ext import tasks
 from dotenv import load_dotenv
 
-# from launcher import Launcher
+from launcher import Launcher
 from index import Friday
 import cogs
 
@@ -45,5 +45,5 @@ class Friday_testing(Friday):
 
 
 def test_will_it_blend():
-  # loop = asyncio.get_event_loop()
-  # Launcher(loop).start()
+  loop = asyncio.get_event_loop()
+  Launcher(loop).start()
