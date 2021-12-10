0740a51510a071ee670db99009334eaff8e5ee77
Brett Anda <brett@brettanda.ca>
2021-12-10 02:07:30 -0700

    BRUHHHH

--- cogs/redditlink.py
+++ cogs/redditlink.py
@@ -7,9 +7,10 @@ import discord
 import youtube_dl
 # import ffmpeg
 from discord.ext import commands
+from typing import Optional
 from typing_extensions import TYPE_CHECKING
 
-from functions import MessageColors, MyContext, embed
+from functions import MessageColors, MyContext, embed, exceptions, cache
 
 # from discord_slash import cog_ext
 
@@ -50,15 +51,40 @@ REDDIT_CLIENT_SECRET = os.environ.get("REDDITCLIENTSECRET")
 REDDIT_PASSWORD = os.environ.get("REDDITPASSWORD")
 
 
+class MustBeAuthor(exceptions.Base):
+  def __init__(self, message="You must be the author of the message to extract the media"):
+    super().__init__(message=message)
+
+
+class NotRedditLink(exceptions.Base):
+  def __init__(self, message="The message must contain a proper Reddit post with media to extract"):
+    super().__init__(message=message)
+
+
+class Config:
+  __slots__ = ("bot", "id", "enabled", )
+
+  @classmethod
+  async def from_record(cls, record, bot):
+    self = cls()
+
+    self.bot = bot
+    self.id: int = int(record["id"], base=10)
+    self.enabled = bool(record["reddit_extract"])
+    return self
+
+
 class redditlink(commands.Cog):
   """Extract the media from Reddit posts with Friday's Reddit command and more"""
 
   def __init__(self, bot: "Bot"):
     self.bot = bot
+
     self.emoji = "🔗"
     self.loop = bot.loop
 
-    self.lock = asyncio.Lock()
+    self.session_lock = asyncio.Lock()
+    self.extract_lock = asyncio.Lock()
     self.reddit = asyncpraw.Reddit(
         client_id=REDDIT_CLIENT_ID,
         client_secret=REDDIT_CLIENT_SECRET,
@@ -67,6 +93,25 @@ class redditlink(commands.Cog):
         username="Motostar19"
     )
     self.reddit.read_only = True
+
+  @cache.cache()
+  async def get_guild_config(self, guild_id: int) -> Optional[Config]:
+    query = "SELECT * FROM servers WHERE id=$1 LIMIT 1;"
+    async with self.bot.db.pool.acquire(timeout=300.0) as conn:
+      record = await conn.fetchrow(query, str(guild_id))
+      self.bot.logger.debug(f"PostgreSQL Query: \"{query}\" + {str(guild_id)}")
+      if record is not None:
+        return await Config.from_record(record, self.bot)
+      return None
+
+  async def cog_command_error(self, ctx: "MyContext", error: Exception):
+    just_send = (MustBeAuthor, NotRedditLink,)
+    error = getattr(error, 'original', error)
+
+    if isinstance(error, just_send):
+      await ctx.send(embed=embed(title=error, color=MessageColors.ERROR))
+    else:
+      raise error
 
   @commands.Cog.listener()
   async def on_message(self, message: discord.Message):
@@ -90,10 +135,9 @@ class redditlink(commands.Cog):
     if len(reg) != 1:
       return
 
-    if message.guild:
-      db_enabled = await self.bot.db.query("SELECT reddit_extract FROM servers WHERE id=$1 LIMIT 1", str(message.guild.id))
-      if not db_enabled:
-        return
+    config = await self.get_guild_config(message.guild.id)
+    if config is None or not config.enabled:
+      return
 
     ctx: "MyContext" = await self.bot.get_context(message)
     if ctx.command is not None:
@@ -151,7 +195,7 @@ class redditlink(commands.Cog):
     guild = self.bot.get_guild(payload.guild_id)
     async with channel.typing():
       try:
-        await self.extract(message.content, payload=payload, guild=guild, channel=channel, message=message)
+        await message.reply(**await self.extract(message.content, payload=payload, guild=guild, channel=channel, message=message))
       except Exception as e:
         await message.reply(embed=embed(title="Something went wrong", description="Please try again later. I have notified my boss of this error", color=MessageColors.ERROR), mention_author=False)
         raise e
@@ -160,17 +204,18 @@ class redditlink(commands.Cog):
   async def norm_extract(self, ctx: "MyContext", link: str):
     try:
       async with ctx.typing():
-        return await self.extract(query=link, command=True, ctx=ctx, guild=ctx.guild, channel=ctx.channel)
+        return await ctx.send(**await self.extract(query=link, command=True, ctx=ctx, guild=ctx.guild, channel=ctx.channel))
     except Exception:
-      return await self.extract(query=link, command=True, ctx=ctx, guild=ctx.guild, channel=ctx.channel)
+      return await ctx.send(**await self.extract(query=link, command=True, ctx=ctx, guild=ctx.guild, channel=ctx.channel))
 
-    return await self.extract(query=link, command=True, ctx=ctx, guild=ctx.guild, channel=ctx.channel)
+    return await ctx.send(**await self.extract(query=link, command=True, ctx=ctx, guild=ctx.guild, channel=ctx.channel))
 
   @norm_extract.command("enable", help="Enable or disabled Friday's reddit link extraction. (When disabled Friday won't react to reddit links.)")
   @commands.guild_only()
   @commands.has_guild_permissions(manage_messages=True)
   async def extract_toggle(self, ctx, enable: bool):
     await self.bot.db.query("UPDATE servers SET reddit_extract=$1 WHERE id=$2", enable, str(ctx.guild.id))
+    self.get_guild_config.invalidate(self, ctx.guild.id)
     if enable:
       return await ctx.send(embed=embed(title="I will now react to Reddit links", description="For me to then extract a reddit link the author of the message must react with the same emoji Friday did.\nFriday also requires add_reaction permissions (if not already) for this to work."))
     await ctx.send(embed=embed(title="I will no longer react to Reddit links.", description="The Reddit extract commands will still work."))
@@ -193,7 +238,7 @@ class redditlink(commands.Cog):
     reg = re.findall(PATTERN, query)
 
     if len(reg) != 1:
-      return await ctx.send(embed=embed(title="That is not a reddit post url", color=MessageColors.ERROR))
+      raise NotRedditLink()
 
     data = await self.reddit.submission(url=reg[0])
 
@@ -202,7 +247,7 @@ class redditlink(commands.Cog):
     if data.media is not None and "reddit_video" in data.media:
       link = data.media["reddit_video"]["hls_url"]
       content_length = 0
-      async with self.lock:
+      async with self.session_lock:
         async with self.bot.session.get(
                 link,
                 headers={
@@ -211,11 +256,12 @@ class redditlink(commands.Cog):
                 }) as r:
           if r.status == 200:
             content_length = r.content_length
-      loop = asyncio.get_event_loop()
-      linkdata = await loop.run_in_executor(None, lambda: ytdl.extract_info(link, content_length))
-      ext = "webm"  # linkdata['ext']
-      video = True
-      # linkdata = await ytdl.extract_info(link, download=True)
+      async with self.extract_lock:
+        loop = asyncio.get_event_loop()
+        linkdata = await loop.run_in_executor(None, lambda: ytdl.extract_info(link, content_length))
+        ext = "webm"  # linkdata['ext']
+        video = True
+        # linkdata = await ytdl.extract_info(link, download=True)
 
       if 'entries' in linkdata:
         # take first item from a playlist
@@ -247,9 +293,9 @@ class redditlink(commands.Cog):
       try:
         # name = f'{linkdata["extractor"]}-{linkdata["id"]}-{linkdata["title"]}.{linkdata["ext"]}'
         name = data.title.split()
-        return await ctx.send(file=discord.File(fp=mp4file, filename=f'friday-bot.com_{"_".join(name)}.{ext}', spoiler=spoiler))
+        return dict(file=discord.File(fp=mp4file, filename=f'friday-bot.com_{"_".join(name)}.{ext}', spoiler=spoiler))
       except discord.HTTPException:
-        return await ctx.send(embed=embed(title="This file is too powerful to be uploaded", description="You will have to open reddit to view this", color=MessageColors.ERROR))
+        return dict(embed=embed(title="This file is too powerful to be uploaded", description="You will have to open reddit to view this", color=MessageColors.ERROR))
       finally:
         try:
           os.remove(mp4file)
@@ -257,9 +303,9 @@ class redditlink(commands.Cog):
           pass
     else:
       if spoiler is True:
-        return await ctx.send(content="||" + link + "||")
+        return dict(content="||" + link + "||")
       else:
-        return await ctx.send(content=link)
+        return dict(content=link)
     # elif reaction.message.channel.nsfw == False and data["over_18"] == False:
 
     # print(len(body))
