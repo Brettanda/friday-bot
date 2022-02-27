eeb1d78aca4ae007ead2c6233eac49e7495725a4
Brett Anda <brett@brettanda.ca>
2022-02-26 17:18:43 -0700

    another context update

--- functions/custom_contexts.py
+++ functions/custom_contexts.py
@@ -166,11 +166,30 @@ class MultiSelectView(discord.ui.View):
 #     except discord.NotFound:
 #       pass
 
+class _ContextDBAcquire:
+  __slots__ = ("ctx", "timeout")
+
+  def __init__(self, ctx, timeout):
+    self.ctx = ctx
+    self.timeout = timeout
+
+  def __await__(self):
+    return self.ctx._acquire(self.timeout).__await__()
+
+  async def __aenter__(self):
+    await self.ctx._acquire(self.timeout)
+    return self.ctx.db
+
+  async def __aexit__(self, *args):
+    await self.ctx.release()
+
 
 class MyContext(Context):
   def __init__(self, *args, **kwargs):
     self.to_bot_channel: int = None
     super().__init__(*args, **kwargs)
+    self.pool: Pool = self.bot.pool
+    self._db = None
 
   def __repr__(self) -> str:
     return "<Context>"
@@ -187,12 +206,39 @@ class MyContext(Context):
     return self.bot.session
 
   @property
-  def db(self) -> Database:
-    return self.bot.db
+  def db(self):
+    return self._db if self._db else self.pool
 
-  @property
-  def pool(self) -> Pool:
-    return self.bot.db.pool
+  async def _acquire(self, timeout):
+    if self._db is None:
+      self._db = await self.pool.acquire(timeout=timeout)
+    return self._db
+
+  def acquire(self, *, timeout=300.0):
+    """Acquires a database connection from the pool. e.g. ::
+          async with ctx.acquire():
+              await ctx.db.execute(...)
+      or: ::
+          await ctx.acquire()
+          try:
+              await ctx.db.execute(...)
+          finally:
+              await ctx.release()
+    """
+    return _ContextDBAcquire(self, timeout)
+
+  async def release(self):
+    """Releases the database connection from the pool.
+      Useful if needed for "long" interactive commands where
+      we want to release the connection and re-acquire later.
+      Otherwise, this is called automatically by the bot.
+      """
+    # from source digging asyncpg source, releasing an already
+    # released connection does nothing
+
+    if self._db is not None:
+      await self.bot.pool.release(self._db)
+      self._db = None
 
   async def prompt(self, message: str, *, timeout: float = 60.0, author_id: Optional[int] = None, **kwargs) -> Optional[bool]:
     author_id = author_id or self.author.id
