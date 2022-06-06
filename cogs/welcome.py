25a99ca17d308c5ee1e58d60ffd4be3ec149d3f1
Brett Anda <brett@brettanda.ca>
2022-06-06 15:18:33 -0600

    type checking welcome cog

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -1,18 +1,20 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional, Union
+
 import discord
 from discord.ext import commands
 
-from functions import embed, MyContext, MessageColors, cache
-
-from typing import Optional
-from typing_extensions import TYPE_CHECKING
+from functions import MessageColors, cache, embed
 
 if TYPE_CHECKING:
-  from index import Friday as Bot
+  from index import Friday
+  from functions.custom_contexts import MyContext, GuildContext
 
 
-def format_message(content: str, user: discord.User, guild: discord.Guild) -> str:
+def format_message(content: str, user: discord.User | discord.Member, guild: discord.Guild) -> str:
   message_variables = [r"{user}", r"{server}"]
-  if any(var in content.lower() for var in message_variables):
+  if content and any(var in content.lower() for var in message_variables):
     for var in message_variables:
       if var == r"{user}":
         content = f"{user.mention}".join(content.split(var))
@@ -22,33 +24,42 @@ def format_message(content: str, user: discord.User, guild: discord.Guild) -> st
 
 
 class Config:
-  __slots__ = ("bot", "id", "channel_id", "role_id", "message",)
+  __slots__ = ("bot", "id", "channel_id", "role_id", "message",
+               )
+
+  bot: Friday
+  id: int
+  channel_id: Optional[int]
+  role_id: Optional[int]
+  message: str
 
   @classmethod
-  def from_record(cls, record, bot):
+  def from_record(cls, record, bot: Friday):
     self = cls()
 
-    self.bot: "Bot" = bot
-    self.id: int = int(record["guild_id"], base=10)
-    self.channel_id: Optional[int] = int(record["channel_id"], base=10) if record["channel_id"] is not None else None
-    self.role_id: Optional[int] = int(record["role_id"], base=10) if record["role_id"] is not None else None
-    self.message: Optional[str] = record["message"]
+    self.bot = bot
+    self.id = int(record["guild_id"], base=10)
+    self.channel_id = int(record["channel_id"], base=10) if record["channel_id"] is not None else None
+    self.role_id = int(record["role_id"], base=10) if record["role_id"] is not None else None
+    self.message = record["message"]
     return self
 
   @property
-  def channel(self) -> Optional[discord.TextChannel]:
-    guild = self.bot.get_guild(self.id)
-    return guild and self.channel_id and guild.get_channel(self.channel_id)
+  def channel(self) -> Optional[Union[discord.TextChannel, discord.VoiceChannel, discord.Thread]]:
+    if self.channel_id:
+      guild = self.bot.get_guild(self.id)
+      return guild and guild.get_channel(self.channel_id)  # type: ignore
 
   @property
   def role(self) -> Optional[discord.Role]:
-    guild = self.bot.get_guild(self.id)
-    return guild and self.role_id and guild.get_role(self.role_id)
+    if self.role_id:
+      guild = self.bot.get_guild(self.id)
+      return guild and guild.get_role(self.role_id)
 
 
 class Welcome(commands.Cog):
-  def __init__(self, bot: "Bot"):
-    self.bot = bot
+  def __init__(self, bot: Friday):
+    self.bot: Friday = bot
 
   def __repr__(self) -> str:
     return f"<cogs.{self.__cog_name__}>"
@@ -56,16 +67,16 @@ class Welcome(commands.Cog):
   def cog_check(self, ctx: MyContext) -> bool:
     return ctx.guild is not None
 
-  async def cog_command_error(self, ctx, error):
+  async def cog_command_error(self, ctx: MyContext, error: commands.CommandError):
     if isinstance(error, commands.CheckFailure):
       raise commands.NoPrivateMessage()
 
   @cache.cache(ignore_kwargs=True)
   async def get_guild_config(self, guild_id: int, *, connection=None) -> Optional[Config]:
-    connection = connection or self.bot.pool
+    conn = connection or self.bot.pool
 
     query = "SELECT * FROM welcome WHERE guild_id=$1 LIMIT 1"
-    record = await connection.fetchrow(query, str(guild_id))
+    record = await conn.fetchrow(query, str(guild_id))
     self.bot.logger.debug(f"PostgreSQL Query: \"{query}\" + {str(guild_id)}")
     if record is None:
       return None
@@ -92,11 +103,11 @@ class Welcome(commands.Cog):
     message, channel = config.message, config.channel
     if message is None or channel is None:
       return
+    message = format_message(message, member, member.guild)
     if not isinstance(config.channel, discord.TextChannel):
       await self.bot.pool.execute("UPDATE welcome SET channel_id=NULL WHERE guild_id=$1", str(member.guild.id))
       self.get_guild_config.invalidate(self, member.guild.id)
       return
-    message = format_message(message, member, member.guild)
     await channel.send(message, allowed_mentions=discord.AllowedMentions(users=True))
 
   async def add_welcome_role(self, member: discord.Member) -> None:
@@ -116,15 +127,15 @@ class Welcome(commands.Cog):
   @commands.group(name="welcome", invoke_without_command=True, case_insensitive=True, help="Friday's settings for welcoming new members to your servers")
   @commands.has_guild_permissions(manage_roles=True, manage_guild=True, manage_channels=True)
   @commands.bot_has_guild_permissions(manage_roles=True)
-  async def _welcome(self, ctx: "MyContext"):
-    return await ctx.invoke(self._welcome_display)
+  async def _welcome(self, ctx: GuildContext):
+    return await ctx.invoke(self._welcome_display)  # type: ignore
 
   @_welcome.command(name="display", aliases=["list", "show"], help="Shows the servers current welcome settings")
   @commands.has_guild_permissions(manage_roles=True, manage_guild=True, manage_channels=True)
-  async def _welcome_display(self, ctx: "MyContext"):
+  async def _welcome_display(self, ctx: GuildContext):
     config = await self.get_guild_config(ctx.guild.id, connection=ctx.db)
     if config is None:
-      return await ctx.send(embed=embed(title="Welcome", description="Welcome is not enabled for this server.", color=MessageColors.ERROR))
+      return await ctx.send(embed=embed(title="Welcome", description="Welcome is not enabled for this server.", color=MessageColors.error()))
     await ctx.reply(embed=embed(
         title="Current Welcome Settings",
         fieldstitle=["Role", "Channel", "Message"],
@@ -135,7 +146,7 @@ class Welcome(commands.Cog):
   @_welcome.command(name="role", extras={"examples": ["@default", "12345678910"]}, help="Set the role that is given to new members when they join the server")
   @commands.has_guild_permissions(manage_roles=True, manage_guild=True, manage_channels=True)
   @commands.bot_has_guild_permissions(manage_roles=True)
-  async def _welcome_role(self, ctx: "MyContext", role: Optional[discord.Role] = None):
+  async def _welcome_role(self, ctx: GuildContext, role: Optional[discord.Role] = None):
     role_id = role and role.id
     await self.bot.pool.execute("INSERT INTO welcome (guild_id,role_id) VALUES ($1,$2) ON CONFLICT(guild_id) DO UPDATE SET role_id=$2", str(ctx.guild.id), str(role_id) if role_id else None)
     self.get_guild_config.invalidate(self, ctx.guild.id)
@@ -145,9 +156,9 @@ class Welcome(commands.Cog):
 
   @_welcome.command(name="channel", extras={"examples": ["#welcome", "#general", "707458929696702525"]}, help="Setup a welcome channel for Friday to welcome new memebers in")
   @commands.has_guild_permissions(manage_guild=True, manage_channels=True)
-  async def _welcome_channel(self, ctx: "MyContext", channel: Optional[discord.TextChannel] = None):
+  async def _welcome_channel(self, ctx: GuildContext, channel: Optional[discord.TextChannel] = None):
     if channel is not None and channel.permissions_for(ctx.guild.me).send_messages is False:
-      return await ctx.reply(embed=embed(title=f"I don't have send_permissions in {channel}", color=MessageColors.ERROR))
+      return await ctx.reply(embed=embed(title=f"I don't have send_permissions in {channel}", color=MessageColors.error()))
     channel_id = channel and channel.id
     await self.bot.pool.execute("INSERT INTO welcome (guild_id,channel_id) VALUES ($1,$2) ON CONFLICT(guild_id) DO UPDATE SET channel_id=$2", str(ctx.guild.id), str(channel_id) if channel_id else None)
     config = await self.get_guild_config(ctx.guild.id, connection=ctx.db)
@@ -159,16 +170,16 @@ class Welcome(commands.Cog):
 
   @_welcome.command(name="message", extras={"examples": [r"Welcome to the server {user}, stay a while!", r"Welcome {user} to {server}", "A new member has joined the server!"]}, help="Set a message to greet new members to your server, message variables are `{user}`,`{server}`")
   @commands.has_guild_permissions(manage_guild=True, manage_channels=True)
-  async def _welcome_message(self, ctx: "MyContext", *, message: Optional[str] = None):
+  async def _welcome_message(self, ctx: GuildContext, *, message: Optional[str] = None):
     if message is not None and len(message) > 255:
-      await ctx.reply(embed=embed(title="Welcome messages can't be longer than 255 characters", color=MessageColors.ERROR))
+      await ctx.reply(embed=embed(title="Welcome messages can't be longer than 255 characters", color=MessageColors.error()))
     await self.bot.pool.execute("INSERT INTO welcome (guild_id,message) VALUES ($1,$2) ON CONFLICT(guild_id) DO UPDATE SET message=$2", str(ctx.guild.id), message)
-    formated_message = format_message(message, ctx.author, ctx.guild)
     config = await self.get_guild_config(ctx.guild.id, connection=ctx.db)
     channel_id = config and config.channel_id
     self.get_guild_config.invalidate(self, ctx.guild.id)
     if message is None:
       return await ctx.send(embed=embed(title="Welcome message removed"))
+    formated_message = format_message(message, ctx.author, ctx.guild)
     await ctx.reply(embed=embed(
         title="This servers welcome message is now",
         fieldstitle=["Raw", "Formatted Message"],
