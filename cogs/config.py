5798961eddf17bded025b5a2e5e25d99aa7f6e6c
Brett Anda <brett@brettanda.ca>
2022-07-10 01:14:09 -0600

    some commented things

--- cogs/config.py
+++ cogs/config.py
@@ -168,10 +168,35 @@ class Config(commands.Cog, command_attrs=dict(extras={"permissions": ["manage_gu
 
     if chat is not None:
       chat.get_guild_config.invalidate(chat, ctx.guild.id)
-    log = self.bot.log
-    if log is not None:
-      log.get_guild_config.invalidate(log, ctx.guild.id)
-    ctx._lang = final_lang, self.bot.languages[final_lang]
+  # @commands.command(name="language", extras={"examples": ["en", "es", "english", "spanish"]}, aliases=["lang"], help="Change the language that I will speak. This currently only applies to the chatbot messages not the commands.")
+  # # @commands.cooldown(1, 3600, commands.BucketType.guild)
+  # @commands.has_guild_permissions(administrator=True)
+  # async def new_language(self, ctx: GuildContext):
+  #   langs = self.bot.languages.items()
+
+  #   confirm = await ctx.multi_select(
+  #       "Select the language you would like me to speak in",
+  #       options=[{
+  #           "label": la[1]["_lang_name"],
+  #           "value": la[0],
+  #           "emoji": la[1]["_lang_emoji"],
+  #           "default": la[0] == ctx.lang[0],
+  #       } for la in langs],
+  #       placeholder=ctx.lang[1]["_lang_name"]
+  #   )
+  #   if not confirm:
+  #     return await ctx.reply(embed=embed(title="Cancelled"))
+
+  #   await ctx.db.execute("UPDATE servers SET lang=$1 WHERE id=$2", confirm[0], str(ctx.guild.id))
+
+  #   chat: Optional[Chat] = self.bot.get_cog("Chat")  # type: ignore
+  #   if chat is not None:
+  #     chat.get_guild_config.invalidate(chat, ctx.guild.id)
+  #   log = self.bot.log
+  #   if log is not None:
+  #     log.get_guild_config.invalidate(log, ctx.guild.id)
+  #   ctx._lang = confirm[0], self.bot.languages[confirm[0]]
+  #   await ctx.reply(embed=embed(title=f"{ctx.lang[1]['config']['lang']['new_lang']}`{self.bot.languages[confirm[0]]['_lang_name']}`"))
 
   @commands.group("botchannel", invoke_without_command=True, case_insensitive=True, extras={"examples": ["#botspam"]}, help="The channel where bot commands live.")
   async def botchannel(self, ctx: GuildContext, *, channel: discord.TextChannel = None):
@@ -265,6 +290,74 @@ class Config(commands.Cog, command_attrs=dict(extras={"permissions": ["manage_gu
   @commands.is_owner()
   async def disable_all(self, ctx: GuildContext):
     ...
+
+  # async def _bulk_ignore_entries(self, ctx: MyContext, entries):
+  #   async with ctx.acquire():
+  #     async with ctx.db.transaction():
+  #       query = "SELECT entity_id FROM plonks WHERE guild_id=$1;"
+  #       records = await ctx.db.fetch(query, ctx.guild.id)
+
+  #       current_plonks = {r[0] for r in records}
+  #       guild_id = ctx.guild.id
+  #       to_insert = [(guild_id, e.id) for e in entries if e.id not in current_plonks]
+
+  #       await ctx.db.copy_records_to_table("plonks", columns=("guild_id", "entity_id"), records=to_insert)
+
+  #       self.is_plonked.invalidate_containing(f"{ctx.guild.id!r}:")
+
+  # @commands.group("modrole", aliases=["modroles"], invoke_without_command=True)
+  # @commands.has_guild_permissions(administrator=True)
+  # async def modrole(self, ctx: GuildContext, *roles: discord.Role):
+  #   """Sets the mod role for the server.
+  #      Members with this role will be able to use ALL mod commands."""
+  #   if not roles:
+  #     db_roles = await ctx.db.fetchval("SELECT mod_roles FROM servers WHERE id=$1;", str(ctx.guild.id))
+  #     db_roles = [ctx.guild.get_role(int(role, base=10)) for role in db_roles]
+  #     db_roles = [role for role in db_roles if role is not None]
+  #     return await ctx.send(embed=embed(title="Mod Roles", description="\n".join(role.mention for role in db_roles) if db_roles else "There are no mod roles."))
+
+  #   query = "UPDATE servers SET mod_roles=$1 WHERE id=$2;"
+  #   await ctx.db.execute(query, [str(role.id) for role in roles], str(ctx.guild.id))
+  #   self.get_guild_config.invalidate(self, ctx.guild.id)
+  #   await ctx.send(embed=embed(title="Mod Roles", description="\n".join(role.mention for role in roles) if roles else "There are no mod roles."))
+
+  # @modrole.command("clear", aliases=["reset"])
+  # @commands.has_guild_permissions(administrator=True)
+  # async def modrole_clear(self, ctx: GuildContext):
+  #   """Clears the mod role for the server."""
+  #   query = "UPDATE servers SET mod_roles=array[]::text[] WHERE id=$1;"
+  #   await ctx.db.execute(query, str(ctx.guild.id))
+  #   self.get_guild_config.invalidate(self, ctx.guild.id)
+  #   await ctx.send(embed=embed(title="Mod Roles", description="There are no mod roles."))
+
+  # @commands.command("canrun", aliases=["diagnose"], help="Let's you know if you can use/run a command in channel.", hidden=True)
+  # # @commands.is_owner()
+  # async def canrun(self, ctx: GuildContext, *, command: Annotated[commands.Command, Command]):
+  #   checkup = {
+  #       True: "\N{WHITE HEAVY CHECK MARK}",
+  #       False: "\N{CROSS MARK}",
+  #       None: "\N{HORIZONTAL ELLIPSIS}"
+  #   }
+  #   e = discord.Embed(title=f"Diagnose for `{command.qualified_name}`", colour=MessageColors.default())
+
+  #   e.add_field(name=f"{checkup[ctx.guild.me.top_role.position > ctx.author.top_role.position]} Role Hierarchy", value="```" + ('Bot\'s role is high enough' if ctx.guild.me.top_role.position > ctx.author.top_role.position else 'Bot\'s role is not high enough') + "```")
+
+  #   required_perms = [("send_messages", True), ("read_messages", True), ("embed_links", True), ("add_reactions", True)]
+  #   me = ctx.guild.me if ctx.guild is not None else ctx.bot.user
+  #   permissions = ctx.channel.permissions_for(me)
+  #   missing = [perm for perm, value in required_perms if getattr(permissions, perm) != value]
+  #   e.add_field(name=f"{checkup[len(missing) == 0]} Required Bot Permissions", value=f"```{', '.join(missing) if missing else 'No missing permissions'}```")
+
+  #   disabled, restricted, botchannel = await ctx.db.fetchrow("SELECT disabled_commands, restricted_commands, botchannel FROM servers WHERE id=$1;", str(ctx.guild.id))
+  #   e.add_field(name=f"{checkup[command.qualified_name not in disabled]} Command Disabled", value=f"```{'Command has not been disabled' if command.qualified_name not in disabled else 'Command is disabled'}```")
+
+  #   botchannel = "#" + ctx.guild.get_channel(int(botchannel, base=10)).name if botchannel else "None"
+  #   e.add_field(name=f"{checkup[command.qualified_name not in restricted]} Command Restricted", value="```" + ('Command has not been restricted' if command.qualified_name not in restricted else ('Command is restricted to ' + botchannel)) + "```")
+
+  #   command_perms = await command.can_run(ctx)
+  #   e.add_field(name=f"{checkup[command_perms]} Command Permissions", value="```" + ('Command has all required permissions' if command_perms else 'Command does not have all required permissions') + "```")
+
+  #   await ctx.send(embed=e)
 
 
 async def setup(bot: Friday):
