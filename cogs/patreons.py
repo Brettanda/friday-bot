e38fc0de4f010cfaa31e00536a27d414cffd5233
Brett Anda <brett@brettanda.ca>
2022-07-26 01:46:58 -0600

    removed the shit ass patreon python api wrapper

--- cogs/patreons.py
+++ cogs/patreons.py
@@ -1,15 +1,14 @@
 from __future__ import annotations
 
-import functools
 import os
 from typing import TYPE_CHECKING, List, Sequence, Optional, Union
 
 import asyncpg
 import discord
-import patreon
 from discord.ext import commands
 
 from functions import MessageColors, cache, checks, config, embed
+from functions.config import PremiumTiersNew
 from functions.custom_contexts import GuildContext
 
 if TYPE_CHECKING:
@@ -27,23 +26,20 @@ class PatreonButtons(discord.ui.View):
 
 
 class PatreonConfig:
-  __slots__ = ("id", "_tier", "_amount_cents", "declined_since", "guild_ids")
+  __slots__ = ("id", "_current_tier", "_amount_cents", "guild_ids")
 
   id: int
-  _tier: int
   _amount_cents: int
-  declined_since: int
   guild_ids: List[int]
 
   @classmethod
-  def from_pledge_record(cls, pledge: patreon.jsonapi.parser.JSONAPIResource, record: asyncpg.Record) -> Self:
+  def from_pledge_record(cls, patron: dict, record: asyncpg.Record) -> Self:
     self = cls()
 
-    self.declined_since = pledge.relationship("patron").attribute("declined_since")
-    self._amount_cents = pledge.relationship("reward").attribute("amount_cents")
-    self.id = int(pledge.relationship("patron").attribute("social_connections")["discord"]["user_id"], base=10)
+    self._current_tier = patron["current_tier"]
+    self._amount_cents = patron["amount_cents"]
+    self.id = int(patron["user_id"], base=10)
 
-    self._tier = record and record["tier"]
     self.guild_ids = record and [int(r, base=10) for r in record["guild_ids"]] or []
 
     return self
@@ -54,16 +50,9 @@ class PatreonConfig:
   def __repr__(self):
     return f"<PatreonConfig id={self.id} tier={self.tier} guilds={len(self.guild_ids)}>"
 
-  # def __hash__(self):
-  #   return hash(self.id, self.tier, *self.guild_ids)
-
   @property
   def tier(self) -> int:
-    if self._tier:
-      return self._tier
-    if self._amount_cents >= 500:
-      return config.PremiumTiersNew.tier_1.value
-    return config.PremiumTiersNew.tier_1.value
+    return PremiumTiersNew.from_patreon_tier(self._current_tier).value
 
   @property
   def max_guilds(self) -> int:
@@ -80,7 +69,6 @@ class Patreons(commands.Cog):
 
   def __init__(self, bot: Friday) -> None:
     self.bot: Friday = bot
-    self.patreon = patreon.API(CREATOR_TOKEN)
 
   def __repr__(self) -> str:
     return f"<cogs.{self.__cog_name__}>"
@@ -88,26 +76,38 @@ class Patreons(commands.Cog):
   @cache.cache()
   async def get_patrons(self, *, connection: Optional[Union[asyncpg.Pool, asyncpg.Connection]] = None) -> Sequence[PatreonConfig]:
     conn = connection or self.bot.pool
-    campaign = await self.bot.loop.run_in_executor(None, self.patreon.fetch_campaign)
-    campaign_id = campaign.data()[0].id()
 
-    all_pledges = []
-    cursor = None
-    # https://www.youtube.com/watch?v=RO6JxDOVwLQ
-    while True:
-      fetch = functools.partial(self.patreon.fetch_page_of_pledges, cursor=cursor, fields={"pledge": ["total_historical_amount_cents", "declined_since"]})
-      pledges_response = await self.bot.loop.run_in_executor(None, fetch, campaign_id, 25)
-      cursor = await self.bot.loop.run_in_executor(None, self.patreon.extract_cursor, pledges_response)
-      all_pledges += pledges_response.data()
-      if cursor is None:
-        break
+    # available options to return ?include=currently_entitled_tiers,address&fields[member]=full_name,is_follower,last_charge_date,last_charge_status,lifetime_support_cents,currently_entitled_amount_cents,patron_status&fields[tier]=amount_cents,created_at,description,discord_role_ids,edited_at,patron_count,published,published_at,requires_shipping,title,url&fields[address]=addressee,city,line_1,line_2,phone_number,postal_code,state
+    # can also be found here https://docs.patreon.com/#get-api-oauth2-v2-campaigns-campaign_id-members
+    s = 'campaigns/5362911/members?include=currently_entitled_tiers,user&fields[user]=url,social_connections&fields[member]=full_name,currently_entitled_amount_cents,patron_status&fields[tier]=amount_cents,discord_role_ids,published_at,requires_shipping,title,url'
+    response = await self.bot.session.get(
+        "https://www.patreon.com/api/oauth2/v2/{}".format(s),
+        headers={
+              'Authorization': "Bearer {}".format(CREATOR_TOKEN),
+              'User-Agent': 'Patreon-Python, version 0.5.0, platform Linux-5.10.102.1-microsoft-standard-WSL2-x86_64-with-glibc2.29',
+
+        })
+    resp = await response.json()
+
+    members: list[dict] = []
+    for shit in resp['data']:
+      if shit['attributes']['patron_status'] != 'active_patron':
+        continue
+      r = {
+          "current_tier": shit["relationships"]["currently_entitled_tiers"]['data'][0]["id"],
+          "amount_cents": shit['attributes']['currently_entitled_amount_cents'],
+      }
+      for i in resp['included']:
+        if i['id'] == shit['relationships']['user']['data']['id']:
+          r['user_id'] = i['attributes']['social_connections']['discord']['user_id']
+      members.append(r)
 
     query = "SELECT * FROM patrons"
     records = await conn.fetch(query)
 
     configs = []
-    for p in all_pledges:
-      record = next((r for r in records if r["user_id"] == p.relationship("patron").attribute("social_connections")["discord"]["user_id"]), None)
+    for p in members:
+      record = next((r for r in records if r["user_id"] == p["user_id"]), None)
       configs.append(PatreonConfig.from_pledge_record(p, record))
 
     return configs
