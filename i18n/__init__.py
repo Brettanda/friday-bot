26c7613e92fa6dde8c66386f6655375b2089b32e
Brett Anda <brett@brettanda.ca>
2022-08-30 18:27:25 -0600

    looking for the "bad word"

--- i18n/__init__.py
+++ i18n/__init__.py
@@ -471,7 +471,7 @@ class DiceDiceParameters(Struct):
 
 
 class DiceDice(AppCommandDefault):
-  command_name: str = "diceroll"
+  command_name: str = "roll"
   help: str = "..."
   response_title: str = "Your total: {total}"
   response_description: str = "Query: {query}\nResult: {result}"
